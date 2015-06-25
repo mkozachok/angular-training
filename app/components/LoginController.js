d1f6375c0f4cbf5d4a3c2eebff4c2a4dc9f8883a
alex-training <alexeduprofile@gmail.com>
2015-06-25 17:45:22 +0300

    Changes up to lesson #4 (Login)

--- app/components/LoginController.js
+++ app/components/LoginController.js
@@ -2,9 +2,18 @@ catApp.controller('LoginController', function($scope, indetityService) {
 		var vm = this;		
 	
 		vm.authenticated = false;
+		vm.userTitle = "";
+		vm.userData = {};
 
-		vm.login = function (user){
+		vm.login = function(user){
 			indetityService.check(user);
-			vm.authenticated = indetityService.currentUser !== null;	
-		};	
+			vm.authenticated = indetityService.currentUser !== null;
+			vm.userTitle = (indetityService.currentUser !== null) ? indetityService.currentUser.Name : "";
+		};
+
+		vm.logout = function(){
+			indetityService.currentUser = null;
+			vm.authenticated = false;
+			vm.userData = null;
+		};
 	});
