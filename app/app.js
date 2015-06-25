643f20ab0a3fd39dccaf46db5a1126bf7c3a146e
Valentyn Vasylevskyy - Home <valentun9@gmail.com>
2015-06-25 14:21:34 +0300

    Added new branch
    Valentyn Vasylevskyy:
    Added new branch

--- app/app.js
+++ app/app.js
@@ -1,4 +1,127 @@
-(function () {
-    "use strict";
-    angular.module("app", ["ui.router"]);
-})();
+﻿var app = angular.module('app', ['ngResource', 'ngRoute']);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.
+      when('/', {
+          templateUrl: 'list.html',
+          controller: 'CatMainCtrl'
+      }).
+      when('/add', {
+          templateUrl: 'add.html',
+          controller: 'CatAddCtrl'
+      }).
+      otherwise({redirectTo: '/'});
+}]);
+
+app.config(['$resourceProvider', function ($resourceProvider) {
+    // Don't strip trailing slashes from calculated URLs
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+
+app.factory('catFactory', function () {
+    //Not used so fat
+
+    //var factory = {};
+    //var cats = [
+    //    { id: 0, name: "Merin", clicks: 0, pic: "/Res/pic1.png" },
+    //    { id: 1, name: "Linda", clicks: 0, pic: "/Res/pic2.png" },
+    //    { id: 2, name: "Jenny", clicks: 0, pic: "/Res/pic3.png" },
+    //    { id: 3, name: "Bobby", clicks: 0, pic: "/Res/pic4.png" },
+    //    { id: 4, name: "Dendy", clicks: 0, pic: "/Res/pic5.png" }
+    //];
+
+    //factory.getCats = function () {
+    //    return cats;
+    //};
+
+    //return factory;
+
+});
+
+app.service('catService',['$resource', function ($resource) {
+    
+    this.res = $resource('/cats/:cat', { cat: '@cat' }, {
+        update: {
+            method: 'PUT'
+        }
+    });
+
+}]);
+
+app.controller('CatMainCtrl', function ($scope, catService, filterFilter) {
+
+    //REST GET cats
+    $scope.getCatList = function () {
+        $scope.cats = catService.res.query();
+        $scope.cats.$promise.then(function () { $scope.selectedCat = $scope.cats[0]; });
+        
+    };
+    
+    //REST GET cat by id
+    $scope.getCat = function (catId) {
+        $scope.oneCat = catService.res.get({ cat: catId });
+    };
+
+    //REST PUT (update) cat by id
+    $scope.updateCat = function (cat) {
+        catService.res.update({
+            'id': cat.id,
+            'name': cat.name,
+            'likes': cat.likes,
+            'clicks': cat.clicks,
+            'pic': cat.pic,
+            'checked': cat.checked
+        });
+    };
+
+    //REST DELETE cat by id
+    $scope.deleteCat = function (catId) {
+        catService.res.delete({ cat: catId }, function () {
+            $scope.getCatList();
+        });
+    };
+
+
+
+
+    $scope.getCatList();
+
+    $scope.orderProp='name';
+
+    $scope.Click = function () {
+        $scope.selectedCat.clicks++;
+    };
+
+    $scope.Like = function () {
+        $scope.selectedCat.likes++;
+    };
+
+    $scope.Dislike = function () {
+        $scope.selectedCat.likes--;
+    };
+
+    $scope.SelectCat = function (cat) {
+        $scope.selectedCat = cat;
+        $scope.selectedCat.checked = true;
+    };
+
+    $scope.filterCats = function (element) {
+        return element.name.includes($scope.searchName === undefined ? "" : $scope.searchName);
+    };
+});
+
+app.controller('CatAddCtrl', function ($scope, catService, $location) {
+    
+    //REST POST (add) cat
+    $scope.addCat = function () {
+        catService.res.save({
+            'id': 'it will be changed on server',
+            'name': $scope.newCat.Name,
+            'likes': '0',
+            'clicks': '0',
+            'pic': $scope.newCat.Pic,
+            'checked': 'false'
+        }, function () { $location.path("/app"); });
+    };
+
+});
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+var cats = require('./json/cats.json');
+console.log(cats);
+console.log("Array length: " + cats.length);
+
 var expressIO = require('express.io'),
     serveStatic = require('serve-static');
 
 var app = expressIO();
 
 app.use(expressIO.cookieParser());
-app.use(expressIO.session({secret: 'monkey'}));
+app.use(expressIO.session({ secret: 'monkey' }));
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 app.http().io();
 app.listen(8000);
@@ -17,14 +27,72 @@ app.get('/', function(req, res) {
 app.use(expressIO.static(__dirname + '/'));
 app.use(expressIO.static(__dirname + '/build'));
 
-app.get('/mentors', function(req, res) {
-    var result = require('./json/mentors.json');
-    res.json(result);
+
+
+app.get('/cats', function (req, res) {
+    //var result = require('./json/cats.json');
+    res.json(cats);
 });
 
-app.get('/mentees', function(req, res) {
-    var result = require('./json/mentees.json');
-    res.json(result);
+app.get('/cats/:id', function (req, res) {
+   // var result = require('./json/cats.json');
+    res.json(cats[req.params.id]);
+});
+
+app.post('/cats', function (req, res) {
+
+    console.log("Atempt to add new cat:");
+    console.log(req.body);
+
+    var highest = cats[cats.length - 1].id;
+    var itemToAdd = req.body;
+    itemToAdd.id = (parseInt(highest) + 1).toString();
+    cats.push(itemToAdd);
+
+    console.log("Saved new cat with id: " + itemToAdd.id);
+    console.log("new data: ");
+    console.log(cats);
+
+    res.send('Saved');
+})
+
+app.put('/cats', function (req, res) {
+    console.log("Atempt to edit a cat:");
+    console.log(req.body);
+
+    var itemToEdit = req.body;
+    for (var i = cats.length - 1; i >= 0; i--) {
+        if (cats[i].id == itemToEdit.id) {
+            cats[i].name = itemToEdit.name;
+            cats[i].likes = itemToEdit.likes;
+            cats[i].clicks = itemToEdit.clicks;
+            cats[i].pic = itemToEdit.pic;
+            cats[i].checked = itemToEdit.checked;
+
+            console.log("Edited");
+            console.log("updated data: " + cats);
+            break;
+        }
+    }
+    res.send('Edited');
+})
+
+// accept DELETE request at /user
+app.delete('/cats/:id', function (req, res) {
+    //var data = require('./json/cats.json');
+
+    for (var i = cats.length - 1; i >= 0; i--) {
+        if (cats[i].id == req.params.id) {
+            cats.splice(i, 1);
+            break;
+        }
+    };
+
+    console.log("Cat with id " + req.params.id + " has been deleted");
+    console.log("Updated data:");
+    console.log(cats);
+
+    res.send('Cat has been deleted');
 });
 
 exports = module.exports = app;
