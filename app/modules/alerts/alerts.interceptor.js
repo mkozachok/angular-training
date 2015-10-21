802234d113bb8d9a45e90cf56596711986ed7692
Nadzeya.Shedava <Nadzeya.Shedava@aerlingus.com>
2015-10-21 11:01:12 +0300

    Added alerts.

--- app/modules/alerts/alerts.interceptor.js
+++ app/modules/alerts/alerts.interceptor.js
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 angular.module('app').factory('alertsInterceptor', function($injector, $location, $q){
     return {
     }
+=======
+angular.module('alertsModule').factory('alertsInterceptor', function($q, alertsService){
+	return {
+		responseError: function(response){
+			alertsService.handleErrorResponse(response);
+			return $q.reject(response);
+		}
+	}
+>>>>>>> 9f00f4e... Added aler service
 });
--- app/modules/alerts/alerts.service.js
+++ app/modules/alerts/alerts.service.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 angular.module('alertsModule').factory('alertService', function() {
     var message = [];
 
@@ -14,4 +15,23 @@ angular.module('alertsModule').factory('alertService', function() {
         addError: 'addError',
         handleErrorResponce: 'handleErrorResponce'
     }
+=======
+angular.module('alertsModule').factory('alertsService', function(){
+	var messages = [];
+
+	function addError(message){
+		messages.push(message);
+		console.log(message);
+	}
+
+	function handleErrorResponse(error){
+		var errorMessage = error.data &&error.data.error;
+		addError(errorMessage);
+	}
+
+	return {
+		addError: addError,
+		handleErrorResponse: handleErrorResponse
+	}
+>>>>>>> 9f00f4e... Added aler service
 });
